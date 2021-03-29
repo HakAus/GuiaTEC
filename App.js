723adbb77753d46a6ef0e8c9f3cad07345cf87d0
Xfighter32 <austinhakanson2@gmail.com>
2021-03-29 16:50:52 -0600

    Inicio de menú de procesos administrativos, inclusión de dropdownlist

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, ScrollView, View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import Icon from "react-native-vector-icons/Feather";
 
-export default function App() {
+const administrativeProcessesSelectionPage = function () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.sectionText}>Procesos administrativos</Text>
+      </View>
+      <DropDownPicker
+        items={[
+          {
+            label: "USA",
+            value: "usa",
+            icon: () => <Icon name="flag" size={18} color="#900" />,
+          },
+          {
+            label: "UK",
+            value: "uk",
+            icon: () => <Icon name="flag" size={18} color="#900" />,
+          },
+          {
+            label: "France",
+            value: "france",
+            icon: () => <Icon name="flag" size={18} color="#900" />,
+          },
+        ]}
+        defaultValue={"usa"}
+        containerStyle={{ height: 40, marginLeft: 50, marginRight: 50 }}
+        style={{ backgroundColor: "#fafafa" }}
+        itemStyle={{
+          justifyContent: "flex-start",
+        }}
+        dropDownStyle={{
+          backgroundColor: "#fafafa",
+        }}
+        onChangeItem={(item) =>
+          this.setState({
+            country: item.value,
+          })
+        }
+      />
+    </ScrollView>
   );
+};
+export default function App() {
+  console.log(typeof "dfd");
+  return administrativeProcessesSelectionPage();
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sectionText: {
+    padding: 20,
+    fontSize: 40,
+    fontFamily: "",
+    fontWeight: "normal",
+    color: "000000",
   },
 });
