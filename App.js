e7a00a681d7cd1ceaa99f948080aaf214f256bf6
Fabian Vives <55892352+fvives99@users.noreply.github.com>
2021-04-10 22:59:06 -0600

    Se agrega el menu institucional + navegacion

--- App.js
+++ App.js
@@ -1,37 +1,15 @@
 import React from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Alert } from 'react-native'
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack'
+import { Text, View, StyleSheet, TouchableOpacity, Alert, Button } from 'react-native'
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
 
-
-
-
-const Stack = createStackNavigator()
-
-const MyStack = () => {
+function HomeScreen({ navigation }) {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Menu_Principal"
-          component={HomeScreen}
-        />
-        <Stack.Screen name="Menu_Institucional" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-
-const Menu_Principal = ({navigation}) => {
-  return (
-    <NavigationContainer>
-    <View style={styles.container}>
+<View style={styles.container}>
       <Text style={styles.title}>GuiaTEC</Text>
 
       <TouchableOpacity
-        //color="#002855" 
+        //color="#002855"
         //title="Procesos administrativos"
         onPress={() => Alert.alert('Hello')}
         style={styles.button}
@@ -43,7 +21,7 @@ const Menu_Principal = ({navigation}) => {
 
 
       <TouchableOpacity
-        //color="#002855" 
+        //color="#002855"
         //title="Procesos administrativos"
         onPress={() => Alert.alert('Hello')}
         style={styles.button}
@@ -54,7 +32,7 @@ const Menu_Principal = ({navigation}) => {
       </TouchableOpacity>
 
       <TouchableOpacity
-        //color="#002855" 
+        //color="#002855"
         //title="Mapa del TEC"
         onPress={() => Alert.alert('Hello')}
         style={styles.button}
@@ -64,7 +42,7 @@ const Menu_Principal = ({navigation}) => {
 
       </TouchableOpacity>
       <TouchableOpacity
-        //color="#002855" 
+        //color="#002855"
         //title="Informacion de contacto"
         onPress={() => Alert.alert('Hello')}
         style={styles.button}
@@ -76,7 +54,7 @@ const Menu_Principal = ({navigation}) => {
 
       <TouchableOpacity
         style={styles.button}
-        onPress={() => navigation.navigate('Menu_Institucional')}
+        onPress={() => navigation.navigate("Menu Principal")}
 
 
       >
@@ -84,22 +62,88 @@ const Menu_Principal = ({navigation}) => {
 
       </TouchableOpacity>
     </View>
-    </NavigationContainer>
-  );
-};
+  )
+}
 
 
+function menu_comida() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>TEC - Cartago</Text>
+      <Text style={styles.title2}>Menu Institucional</Text>
 
 
+      <TouchableOpacity
+        //color="#002855"
+        //title="Procesos administrativos"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button2}
 
-const Menu_Institucional = () => {
+      >
+        <Text style={styles.buttontext2} >Mañana -- 8am a 10am {"\n"}Café ₡200   Pinto ₡300 {"\n"}Fresco ₡200   Fruta ₡200 </Text>
+
+      </TouchableOpacity>
+
+
+      <TouchableOpacity
+        //color="#002855"
+        //title="Procesos administrativos"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button2}
+
+      >
+        <Text style={styles.buttontext2} >Tarde -- 11am a 2pm{"\n"}Sopa azteca ₡800{"\n"}Arroz ₡100 Frijoles ₡200{"\n"}Fresco ₡200 Fruta ₡200</Text>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        //color="#002855"
+        //title="Mapa del TEC"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button2}
+
+      >
+        <Text style={styles.buttontext2} >Merienda 3pm{"\n"}Café ₡200   Arepa ₡200{"\n"}Fresco ₡200   Fruta ₡200</Text>
+
+      </TouchableOpacity>
+      <TouchableOpacity
+        //color="#002855"
+        //title="Informacion de contacto"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button2}
+
+      >
+        <Text style={styles.buttontext2} >Noche -- 5pm a 6:30pm{"\n"}Pollo en salsa caribeña ₡800{"\n"}Arroz ₡100  Frijoles ₡200{"\n"}Fresco ₡200  Fruta ₡200</Text>
+
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
+
+export default function App() {
   return (
     <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Menu Principal" component={menu_comida} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+
+
+
+}
+
+
+/*const app = () => {
+  return (
     <View style={styles.container}>
       <Text style={styles.title}>GuiaTEC</Text>
 
       <TouchableOpacity
-        //color="#002855" 
+        //color="#002855"
         //title="Procesos administrativos"
         onPress={() => Alert.alert('Hello')}
         style={styles.button}
@@ -108,33 +152,108 @@ const Menu_Institucional = () => {
         <Text style={styles.buttontext} >Procesos administrativos</Text>
 
       </TouchableOpacity>
+
+
+      <TouchableOpacity
+        //color="#002855"
+        //title="Procesos administrativos"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button}
+
+      >
+        <Text style={styles.buttontext} >Horario de buses </Text>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        //color="#002855"
+        //title="Mapa del TEC"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button}
+
+      >
+        <Text style={styles.buttontext} >Mapa del TEC </Text>
+
+      </TouchableOpacity>
+      <TouchableOpacity
+        //color="#002855"
+        //title="Informacion de contacto"
+        onPress={() => Alert.alert('Hello')}
+        style={styles.button}
+
+      >
+        <Text style={styles.buttontext} >Informacion de contacto </Text>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert('Hello')}
+
+
+      >
+        <Text style={styles.buttontext} >Menu Institucional </Text>
+
+      </TouchableOpacity>
     </View>
-    </NavigationContainer>
   );
 };
+
+
+
+*/
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#FFFFFF" },
   title: { fontSize: 40, color: "black" },
+  title2: { fontSize: 20, color: "black" },
   button: {
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: "#002855",
     padding: 7,
     marginTop: 50,
-    alignSelf: 'stretch'
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 80,
+    width:"98%"
+  },
+
+
+
+
+  button2: {
+    backgroundColor: "#002855",
+    padding: 7,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    borderWidth:2,
+    padding :10,
+    width:"90%"
   },
 
   buttontext: {
     justifyContent: 'center',
     alignItems: 'center',
-    
     color: "white",
     fontSize: 30
 
 
 
   }
+,
+
+
+  buttontext2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: "white",
+    fontSize: 22,
+
+
+
+  }
 });
 
-export default Menu_principal;
+//export default app;
+
